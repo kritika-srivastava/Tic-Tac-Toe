3dd0b4dc92c2ad34facdd2b6e38a888f04c62eab
kritika-srivastava <kritika.s1408@gmail.com>
2020-08-07 10:35:49 +0530

    Mini game Updated.

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -5,73 +5,98 @@ import java.util.Scanner;
 public class Main {
     public static void main(String Args[]) {
         Scanner kb = new Scanner(System.in);
-        // ---------------------------------Input
-        System.out.println("Enter cells:");
-        String s = kb.nextLine();
-        String[] L = new String[3];
-        L[0] = s.substring(0, 3);
-        L[1] = s.substring(3, 6);
-        L[2] = s.substring(6);
 
-        int k = 0, x = 0, o = 0;
         char[][] m = new char[3][3];
 
         for (int i = 0; i < 3; i++) {
 
             for (int j = 0; j < 3; j++) {
-                m[i][j] = L[i].charAt(j);
+                m[i][j] = '_';
             }
         }
         Print(m);
 
-        /*
-         * ---------------------------------Part-2 char win = ' '; int l = 0; if
-         * (m[0][0] == m[1][1] && m[1][1] == m[2][2]) { win = m[0][0]; l++; } else if
-         * (m[0][2] == m[1][1] && m[1][1] == m[2][0]) { win = m[0][0]; l++; } else { for
-         * (int i = 0; i < 3; i++) { if (m[i][0] == m[i][1] && m[i][1] == m[i][2]) { win
-         * = m[i][0]; l++; } else if (m[0][i] == m[1][i] && m[1][i] == m[2][i]) { win =
-         * m[0][i]; l++; } else {
-         * 
-         * } } }
-         * 
-         * if (Math.abs(x - o) >= 2) { System.out.println("Impossible"); } else if (l ==
-         * 0 && k == 9) { System.out.println("Draw"); } else if (l == 1) {
-         * System.out.println(win + " wins"); } else if (l > 1) {
-         * System.out.println("Impossible"); } else if (k != 9) {
-         * System.out.println("Game not finished"); } else {
-         * System.out.println("Impossible"); }
-         */
-
-        // ---------------------------------Part-3
+        int k = 1, x = 0, o = 0, out = 0;
+        ;
 
         int xx = -1, yy = -1;
-        while (xx == -1 && yy == -1) {
-            System.out.println("Enter the coordinates:");
+        wd: while (out != 1) {
+            while (xx == -1 && yy == -1) {
+                System.out.println("Enter the coordinates:");
 
-            String c = kb.nextLine();
-            
-            if (Character.isDigit(c.charAt(0)) == true && Character.isDigit(c.charAt(2)) == true) {
-                String[] cc=c.split(" ");
-                xx = Integer.parseInt(cc[0]);
-                yy = Integer.parseInt(cc[1]);
-                if ((xx <= 3 && xx >= 1) && (yy <= 3 && yy >= 1)) {
-                    if (m[3 - yy ][xx - 1] == '_') {
-                        m[3 - yy ][xx - 1] = 'X';
-                        Print(m);
+                String c = kb.nextLine();
+
+                if (Character.isDigit(c.charAt(0)) == true && Character.isDigit(c.charAt(2)) == true) {
+                    String[] cc = c.split(" ");
+                    xx = Integer.parseInt(cc[0]);
+                    yy = Integer.parseInt(cc[1]);
+                    if ((xx <= 3 && xx >= 1) && (yy <= 3 && yy >= 1)) {
+                        if (m[3 - yy][xx - 1] == '_') {
+                            if (k % 2 == 1) {
+                                m[3 - yy][xx - 1] = 'X';
+                                x++;
+                            } else {
+                                m[3 - yy][xx - 1] = 'O';
+                                o++;
+                            }
+                            Print(m);
+
+                            char win = ' ';
+                            int l = 0;
+                            if (m[0][0] == m[1][1] && m[1][1] == m[2][2] && m[0][0] != '_') {
+                                win = m[0][0];
+                                l++;
+                            } else if (m[0][2] == m[1][1] && m[1][1] == m[2][0] && m[0][2] != '_') {
+                                win = m[0][0];
+                                l++;
+                            } else {
+                                for (int i = 0; i < 3; i++) {
+                                    if (m[i][0] == m[i][1] && m[i][1] == m[i][2] && m[i][0] != '_') {
+                                        win = m[i][0];
+                                        l++;
+                                    } else if (m[0][i] == m[1][i] && m[1][i] == m[2][i] && m[0][i] != '_') {
+                                        win = m[0][i];
+                                        l++;
+                                    } else {
+                                    }
+                                }
+                            }
+                            if (Math.abs(x - o) >= 2) {
+                                System.out.println("Impossible");
+                                out = 1;
+                            } else if (l == 0 && k == 9) {
+                                System.out.println("Draw");
+                                out = 1;
+                            } else if (l == 1) {
+                                System.out.println(win + " wins");
+                                out = 1;
+                            } else if (l > 1) {
+                                System.out.println("Impossible");
+                                out = 1;
+                            } else if (k != 9) {
+                                k++;
+                                xx = -1;
+                                yy = -1;
+                                continue wd;
+                            } else {
+                                System.out.println("Impossible");
+                                out = 1;
+                            }
+                        } else {
+                            System.out.println("This cell is occupied! Choose another one!");
+                            xx = -1;
+                            yy = -1;
+                        }
                     } else {
-                        System.out.println("This cell is occupied! Choose another one!");
+                        System.out.println("Coordinates should be from 1 to 3!");
                         xx = -1;
                         yy = -1;
                     }
                 } else {
-                    System.out.println("Coordinates should be from 1 to 3!");
-                    xx = -1;
-                    yy = -1;
+                    System.out.println("You should enter numbers!");
                 }
-            } else {
-                System.out.println("You should enter numbers!");
-            }
 
+            }
         }
     }
 
